ad0639e04acd9d906c6464ae01e664408d1dc367
FinkoAI <finkoai@sibhtc.ru>
2016-05-13 20:39:26 +0600

    Commit with base functionality

---
+++ AcadPluginTest/AcadPluginTest/View/PropertiesDialog.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace AcadPluginTest.View
+{
+    /// <summary>
+    /// Code behind for PropertiesDialog.xaml
+    /// </summary>
+    public partial class PropertiesDialog : Window
+    {
+        public PropertiesDialog()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ AcadPluginTest/AcadPluginTest/ViewModel/PropertiesDialogViewModel.cs
@@ -0,0 +1,15 @@
+﻿using GalaSoft.MvvmLight;
+
+namespace AcadPluginTest.ViewModel
+{
+    public class PropertiesDialogViewModel : ViewModelBase
+    {
+        private string _text;
+
+        public string Text
+        {
+            get { return _text; }
+            set { Set(() => Text, ref _text, value); }
+        }
+    }
+}
